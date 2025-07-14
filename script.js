4c49696f5ae34827c7fd388ff97321d61974084d
SRINIVAS RYAKALA <143197882+Srinu467@users.noreply.github.com>
2025-07-14 13:51:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Add event on multiple or single elements
+ */
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * Declare elements
+ */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+/**
+ * Activate header and back to top button on scroll
+ */
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
